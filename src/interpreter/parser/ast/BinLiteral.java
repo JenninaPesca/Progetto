0ee0314d7850a4b6a6ad57d50c55267806e2c41b
craffen <mattiacaruso1995@gmail.com>
2018-08-24 14:28:58 +0200

    INT TO BIN

--- src/interpreter/parser/ast/BinLiteral.java
+++ src/interpreter/parser/ast/BinLiteral.java
@@ -1,25 +1,46 @@
 package interpreter.parser.ast;
 
+import interpreter.parser.ast.PrimLiteral;
 import interpreter.visitors.Visitor;
 
-public class BinLiteral extends PrimLiteral<Integer>{
+public class BinLiteral extends PrimLiteral<Integer> {
 
-	String pref = "0b";// modifica
+	String pref = "0b"; //modifica: salvare con il metodo che salva n  anche la prima parte della stringa
 	public BinLiteral(int n) {
 		super(n);
 	}
-
+	
+	private String intToBin(int n) {
+		String num="";
+		int aux=n;
+		while(aux!=0){
+			if((aux%2) == 0){
+				System.out.println(aux);
+				num += "0";
+			}
+				
+			else 
+				num += "1";
+		}
+		return num;
+	}
+	/*modifica: non funzionaaaaaaaa*/
+	
+	@Override
+	public String toString() {
+		return  getClass().getSimpleName() + " (" + this.intToBin(value) + ")"; /*modifica: sbagliato*/
+	}
+	
 	@Override
 	public <T> T accept(Visitor<T> visitor) {
-		// TODO Auto-generated method stub
-		return null;
+		return visitor.visitIntLiteral(value);
 	}
 	
-	@Override
-	public String toString(){
-		return getClass().getSimpleName() + "(" + value + ")";
+	public static void main(String[] args) {
+		BinLiteral p = new BinLiteral(5);
+		System.out.println(p.toString());
 	}
-	
+
 }
 
 
