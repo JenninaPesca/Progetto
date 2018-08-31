a3c346bf72441067989e2f730b1682c9363ea007
JenninaPesca <JenninaPesca@users.noreply.github.com>
2018-08-31 10:41:55 +0200

    impostazione

--- src/interpreter/visitors/evaluation/Value.java
+++ src/interpreter/visitors/evaluation/Value.java
@@ -9,5 +9,16 @@ public interface Value {
 	default ListValue asList() {
 		throw new EvaluatorException("Expecting a list value");
 	}
-
+	//fatto da me 
+	default BoolValue asBool() {
+		throw new EvaluatorException("Expecting a bool value");
+	}
+	
+	default EmptyOptValue asEmptyOptValue() {
+		throw new EvaluatorException("Expecting a EmptyOpt value");
+	}
+	
+	default OptValue asOptValue() {
+		throw new EvaluatorException("Expecting a Opt value");
+	}
 }
