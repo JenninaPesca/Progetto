9596ba843adaefab6fb55f79af80dbb041626633
JenninaPesca <JenninaPesca@users.noreply.github.com>
2018-08-31 08:08:45 +0200

    ordine

--- src/interpreter/visitors/Visitor.java
+++ src/interpreter/visitors/Visitor.java
@@ -7,57 +7,59 @@ import interpreter.parser.ast.Stmt;
 import interpreter.parser.ast.StmtSeq;
 
 public interface Visitor<T> {
-	T visitAdd(Exp left, Exp right);
-
-	T visitAssignStmt(Ident ident, Exp exp);
-
-	T visitForEachStmt(Ident ident, Exp exp, StmtSeq block);
-
-	T visitIfThenStmt(Exp exp, StmtSeq then_seq); //v
-	
-	T visitIfThenElseStmt(Exp exp, StmtSeq then_seq, StmtSeq else_seq); //v
-	
-	T visitDoWhileStmt(StmtSeq block, Exp exp); //v
-	
-	T visitBoolLiteral(boolean value); //v
+	/*prog*/
+	T visitProg(StmtSeq stmtSeq);
+	/*literal*/	
+	T visitListLiteral(ExpSeq exps);
 	
 	T visitIntLiteral(int value);
-
-	T visitListLiteral(ExpSeq exps);
-
-	T visitMoreExp(Exp first, ExpSeq rest);
-
-	T visitMoreStmt(Stmt first, StmtSeq rest);
-
+	
+	T visitIdent(String name);
+	
+	T visitBoolLiteral(boolean value); //fatto da me
+	/*operatori binari*/
+	T visitAdd(Exp left, Exp right);
+	
 	T visitMul(Exp left, Exp right);
 
 	T visitPrefix(Exp left, Exp right);
 	
-	T visitAnd(Exp left, Exp right); //v
+	T visitAnd(Exp left, Exp right); //fatto da me
 	
-	T visitEq(Exp left, Exp right); //v
-
-	T visitPrintStmt(Exp exp);
-
-	T visitProg(StmtSeq stmtSeq);
-
+	T visitEq(Exp left, Exp right); //fatto da me
+	/*operatori unari*/
 	T visitSign(Exp exp);
 	
-	T visitNot(Exp exp); //v
+	T visitNot(Exp exp); //fatto da me
 	
-	T visitEmpty(Exp exp); //v
+	T visitOpt(Exp exp); //fatto da me
 	
-	T visitOpt(Exp exp); //v
+	T visitEmpty(Exp exp); //fatto da me
 	
-	T visitGet(Exp exp); //v
+	T visitDef(Exp exp); //fatto da me
 	
-	T visitDef(Exp exp); //v
-
-	T visitIdent(String name);
-
+	T visitGet(Exp exp); //fatto da me
+	/*sequence of exp*/	
 	T visitSingleExp(Exp exp);
 
-	T visitSingleStmt(Stmt stmt);
+	T visitMoreExp(Exp first, ExpSeq rest);	
 
+	/*Stmt*/
+	T visitAssignStmt(Ident ident, Exp exp);
+	
 	T visitVarStmt(Ident ident, Exp exp);
+	
+	T visitPrintStmt(Exp exp);
+	
+	T visitForEachStmt(Ident ident, Exp exp, StmtSeq block);
+
+	T visitIfThenStmt(Exp exp, StmtSeq then_seq); //fatto da me
+	
+	T visitIfThenElseStmt(Exp exp, StmtSeq then_seq, StmtSeq else_seq); //fatto da me
+	
+	T visitDoWhileStmt(StmtSeq block, Exp exp); //fatto da me
+	/*sequence of stmt*/
+	T visitSingleStmt(Stmt stmt);
+	
+	T visitMoreStmt(Stmt first, StmtSeq rest);
 }
